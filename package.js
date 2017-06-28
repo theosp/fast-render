069bbbffd4d2b729360728009494edcfe91b743a
Daniel C <daniel@meteorspark.com>
2017-06-28 15:17:18 +0300

    Make localstorage an explicit dependency

--- package.js
+++ package.js
@@ -42,6 +42,7 @@ Package.onTest(function(api) {
 
 function configure (api) {
   api.versionsFrom('METEOR@0.9.3');
+  api.use('localstorage', 'client');
   api.use('meteorhacks:inject-data@2.0.0', ['client', 'server']);
   api.use('iron:router@0.9.0 || 1.0.0', ['client', 'server'], {weak: true});
   api.use('chuangbo:cookie@1.1.0', 'client');
